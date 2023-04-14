21c9bc97e0fbdbdf8c3c84aff78047c7191413f2
Sinh LA <phancongsinha3@gmail.com>
2023-04-14 22:13:38 +0700

    fix viewmore button

--- src/components/main/Products/ViewMoreButton.js
+++ src/components/main/Products/ViewMoreButton.js
@@ -1,40 +1,32 @@
-import { memo, useContext, useState } from 'react';
-import { FilterContext, FilterDispatchContext } from '../Main';
-import { initDataAction } from '../../../logic/filter_reducer/actions';
+import { memo, useContext, useState } from "react";
+import { FilterContext, FilterDispatchContext } from "../Main";
+import { initDataAction } from "../../../logic/filter_reducer/actions";
 
 function ViewMoreButton() {
   const data = useContext(FilterContext);
   const filterDispatch = useContext(FilterDispatchContext);
   const [noMore, setNoMore] = useState(false);
-  let cssClass = noMore ? 'view_more_btn hide' : 'view_more_btn';
+  let cssClass = noMore ? "view_more_btn hide" : "view_more_btn";
 
   const clickHandle = async () => {
     data.page += 1;
-    const url = `${data.url}page=${data.page}`;
+    const url = `${data.url}&page=${data.page}`;
 
     await fetch(url)
       .then((res) => res.json())
       .then((fetchData) => {
-        if (fetchData.listDefault.list.length > 0) {
+        const lengthData = fetchData.listDefault.list.length;
+        if (lengthData >= 0) {
           const newData = {
             ...data,
             listDefault: {
               list: [...data.listDefault.list, ...fetchData.listDefault.list],
             },
-            attributeSpecItems: [
-              ...data.attributeSpecItems,
-              ...fetchData.attributeSpecItems,
-            ],
-            promotionItems: [
-              ...data.promotionItems,
-              ...fetchData.promotionItems,
-            ],
           };
 
           filterDispatch(initDataAction(newData));
-        } else {
-          setNoMore(true);
         }
+        if (lengthData < 27) setNoMore(true);
       });
   };
   return (
